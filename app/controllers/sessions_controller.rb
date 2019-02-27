38c97d5a2528ea25e08378e487859abaf35addb9
Joey Cho <cjy12182000@naver.com>
2019-02-27 11:47:54 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 require 'pry'
 class SessionsController < ApplicationController
   before_action :require_login
-#  skip_before_action :require_login, only: [:create]
   def create
     session[:name]=params[:name]
   end
