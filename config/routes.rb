edc4f961995f5b26eacc54d7b627d959b9eb3295
Joey Cho <cjy12182000@naver.com>
2019-02-27 11:26:10 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'sessions#new'
-  post '/create', to: 'sessions#create'
+  post 'sessions/create', to: 'sessions#create'
   get 'secrets/show', to: 'secrets#show'
 
 
