3261f5b3a339abe18f6f236ff64bb41b895b7373
Joey Cho <cjy12182000@naver.com>
2019-02-27 10:53:04 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,2 +1,13 @@
 class SecretsController < ApplicationController
+  before_action :require_login
+  def show
+    if session[:name]==nil
+      redirect_to '/'
+  end
+  
+  private
+ 
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
 end
