ecd15b5e2e7f1601ea796d2766de2c3580769164
Joey Cho <cjy12182000@naver.com>
2019-02-27 11:47:47 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,12 @@ class SessionsController < ApplicationController
   before_action :require_login
 #  skip_before_action :require_login, only: [:create]
   def create
-    binding.pry
     session[:name]=params[:name]
   end
 
   private
 
   def require_login
-    binding.pry
     return redirect_to '/' if (!params.include? :name) || params[:name]==""
   end
 end
