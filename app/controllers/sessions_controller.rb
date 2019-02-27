5524fa7421c5afc197912c900cc6aae4f04dcb7b
Joey Cho <cjy12182000@naver.com>
2019-02-27 11:09:34 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   before_action :require_login
   def create
-
+    session[:name]=params[:name]
   end
 
   private
 
   def require_login
-    return redirect_to '/' unless session.include? :name && session[:name]==""
+    return redirect_to '/' unless params.include? :name && params[:name]==""
   end
 end
